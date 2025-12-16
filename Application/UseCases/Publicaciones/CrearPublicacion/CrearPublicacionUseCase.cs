51c2ac8cbbff0aa3f17bcc8aeb3081b76e2517d2
Urielito1031 <urielestebanyurquina@gmail.com>
2025-12-16 00:36:31 -0300

    agregando crearPublicacionUseCase con repositorio

--- Application/UseCases/Publicaciones/CrearPublicacion/CrearPublicacionUseCase.cs
+++ Application/UseCases/Publicaciones/CrearPublicacion/CrearPublicacionUseCase.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain.Entities;
+using Domain.Enums;
+using Domain.Interfaces.Repositories;
 
 namespace Application.UseCases.Publicaciones.CrearPublicacion
 {
    public class CrearPublicacionUseCase
    {
+      private readonly IPublicacionRepository _repository;
+
+      public CrearPublicacionUseCase(IPublicacionRepository repository)
+      {
+         _repository = repository;
+      }
+      public async Task<Guid> EjecutarAsync(
+         Guid usuarioId,
+         Mascota mascota,
+         Ubicacion ubicacion,
+         string descripcion,
+         DateTime fechaPerdido,
+         EstadoMascota estadoMascota) 
+      {
+         var publicacion = Publicacion.Crear(
+            usuarioId,
+            mascota,
+            ubicacion,
+            descripcion, 
+            fechaPerdido, 
+            estadoMascota
+            );
+        
+         await _repository.AgregarAsync( publicacion );
+
+         return publicacion.Id; 
+      }
+
    }
 }
---
+++ Infraestructure/Repositories/PublicacionRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Domain.Interfaces.Repositories;
+using Infraestructure.Data.Contexts;
+
+namespace Infraestructure.Repositories
+{
+   public class PublicacionRepository : IPublicacionRepository
+   {
+
+
+      private readonly AppDbContext _context;
+
+      public PublicacionRepository(AppDbContext context)
+      {
+         _context = context;
+      }
+
+      public async Task AgregarAsync(Publicacion publicacion)
+      {
+         await _context.Publicaciones.AddAsync(publicacion);
+         await _context.SaveChangesAsync();
+      }
+   }
+}
